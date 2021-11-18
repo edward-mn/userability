3b8698cc7bc8edacafd66eb9222be0d980a18d9d
Edward Moreira <edward.moreira@yahoo.com>
2021-11-18 11:58:58 -0300

    User Ability APP
    Starting and Set DB properties :heavy_check_mark:

---
+++ src/main/java/com/spring/userability/UsersAbilityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.userability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAbilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAbilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/userability/UsersAbilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.userability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAbilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
