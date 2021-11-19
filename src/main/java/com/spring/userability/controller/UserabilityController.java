fb4983162a92400e43cecd23727195e10a5194dc
Edward Moreira <edward.moreira@yahoo.com>
2021-11-19 17:04:06 -0300

    User Ability APP
    Finalize post method :heavy_check_mark:

--- src/main/java/com/spring/userability/controller/UserabilityController.java
+++ src/main/java/com/spring/userability/controller/UserabilityController.java
@@ -4,10 +4,14 @@ import com.spring.userability.model.User;
 import com.spring.userability.service.UserabilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -22,5 +26,21 @@ public class UserabilityController {
     List<User> allUsers = userService.findAll();
     mv.addObject("users", allUsers);
     return mv;
+  }
+
+  @RequestMapping(value = "/newuser", method = RequestMethod.GET)
+  public String getNewUser(){
+    return "newUser";
+  }
+
+  @RequestMapping(value = "/newuser", method = RequestMethod.POST)
+  public String saveUser(@Valid User user, BindingResult result, RedirectAttributes attributes){
+    if(result.hasErrors()){
+      return "redirect:/newuser";
+    }
+    user.setDate(LocalDate.now());
+    userService.save(user);
+
+    return "redirect:/users";
   }
 }
