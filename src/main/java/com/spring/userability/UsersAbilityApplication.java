ea073431107ed1c26076382cf0f8a9498b3ad367
Edward Moreira <edward.moreira@yahoo.com>
2021-11-22 21:10:53 -0300

    User Ability APP
    Refactor data to heroku DB :heavy_check_mark:

--- src/main/java/com/spring/userability/UsersAbilityApplication.java
+++ src/main/java/com/spring/userability/UsersAbilityApplication.java
@@ -1,25 +1,13 @@
 package com.spring.userability;
 
-import com.spring.userability.utils.FakeData;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsersAbilityApplication implements CommandLineRunner {
+public class UsersAbilityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersAbilityApplication.class, args);
 	}
-
-	@Autowired
-	FakeData fakedata;
-
-	@Override
-	public void run(String... args) throws Exception {
-		fakedata.savePosts();
-	}
-
 
 }
